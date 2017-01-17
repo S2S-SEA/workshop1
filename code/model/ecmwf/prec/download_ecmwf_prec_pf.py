e49d10370fc3af82a6ca2ec913d0f90321908fff
Raizan <raizan.rahmat@gmail.com>
2017-01-17 22:09:54 +0800

    Uploaded ecmwf precip files

---
+++ code/model/ecmwf/prec/download_ecmwf_prec_pf.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+'''
+This program downloads the ECMWF hindcast data using MARS.
+The example is for Nov 2016 run for all model output that
+fall within Nov for perturbed forecast ('pf'). 
+Choice of 1998-2014 for download corresponds to TRMM data 
+available for the verification later.
+'''
+from subprocess import call # This library needed to make system call
+from ecmwfapi import ECMWFDataServer # Load the ECMWF API library
+server = ECMWFDataServer()
+ 
+# Define data folder
+dest_dir = '../../../../data/model/ecmwf/prec/'
+# All the initial dates that have full weeks in Nov
+init_date = ['10-13','10-17','10-20','10-24','10-27','10-31','11-03','11-07','11-10','11-14','11-17','11-21','11-24']
+
+# For each initial date
+for i in range(0, len(init_date)):
+    server.retrieve({
+        "class": "s2",
+        "dataset": "s2s",
+        "date": "2016-" + init_date[i],
+        "expver": "prod",
+        "hdate": "2014-" + init_date[i] + "/2013-" + init_date[i] + "/2012-" + init_date[i] + "/2011-" + init_date[i] + "/2010-" + init_date[i] + "/2009-" + init_date[i] + "/2008-" + init_date[i] + "/2007-" + init_date[i] + "/2006-" + init_date[i] + "/2005-" + init_date[i] + "/2004-" + init_date[i] + "/2003-" + init_date[i] + "/2002-" + init_date[i] + "/2001-" + init_date[i] + "/2000-" + init_date[i] + "/1999-" + init_date[i] + "/1998-" + init_date[i],
+        "levtype": "sfc",
+        "model": "glob",
+        "number": "1/2/3/4/5/6/7/8/9/10",
+        "origin": "ecmf",
+        "param": "228228",
+        "step": "0/168/336/504/672",
+        "area": "30/80/-20/150", 
+        "stream": "enfh",
+        "target": dest_dir + "ECMWF_prec_2016-" + init_date[i] + "_pf.grib",
+        "time": "00:00:00",
+        "type": "pf",
+    })
+    
+    # Convert from grib to netcdf using ECMWF Grib API 
+    call("grib_to_netcdf -M -I method,type,stream,refdate -T -o " + dest_dir + "ECMWF_prec_2016-" + init_date[i] + "_pf.nc " + dest_dir + "ECMWF_prec_2016-" + init_date[i] + "_pf.grib", shell=True)
+    # Remove the grib file after download
+    call("rm -rf " + dest_dir + "ECMWF_prec_2016-" + init_date[i] + "_pf.grib", shell=True)
---
+++ code/model/ecmwf/prec/process_ecmwf_pfcf.py
@@ -0,0 +1,108 @@
+'''
+This program processes the downloaded perturbed forecast (pf) and control
+forecast dataset (cf). It combines the 10 ensembles members of ECMWF's pf 
+with the 1 ECMWF's cf. The ensemble members are then averaged to provide
+a single ensemble mean value. The precipitation forecast for each week
+(lead time) is calculated by substracting the end points of each time step
+for the weekly total and then dividing by seven to get the daily average
+for each week. The files are then saved to an output netCDF file for the
+next stage of processing. 
+'''
+import netCDF4
+import numpy as np
+import datetime
+
+# Define data folder
+dest_dir = '../../../../data/model/ecmwf/prec/'
+
+# All the initial dates with complete 7-day week in Nov for the 2016 runs
+init_date = ['10-13','10-17','10-20','10-24','10-27','10-31','11-03','11-07','11-10','11-14','11-17','11-21','11-24']
+
+# For each initial date file
+for i_date in range(0,len(init_date)):
+    print("Processing initial date:",init_date[i_date])
+    ds_pf = netCDF4.Dataset(dest_dir + "ECMWF_prec_2016-" + init_date[i_date] + '_pf.nc') # Load the pf file
+    ds_cf = netCDF4.Dataset(dest_dir + "ECMWF_prec_2016-" + init_date[i_date] + '_cf.nc') # Load the cf file
+
+    # You may want to uncomment the following lines
+    # to print out the file and variable information
+    #print(ds_pf.file_format)
+    #print(ds_pf.dimensions.keys())
+    #print(ds_pf.variables)
+
+    # Read in the variables from cf
+    prec_hd = ds_cf.variables['hdate'][:]
+    prec_st = ds_cf.variables['step'][:]
+    prec_lat = ds_cf.variables['latitude'][:]
+    prec_lon = ds_cf.variables['longitude'][:]
+  
+    prec_pf = ds_pf.variables['tp'][:]
+    prec_cf = ds_cf.variables['tp'][:]
+
+    # Read in the array shape from pf's total precip ('tp') variable
+    arr_pf = prec_pf
+
+    arr_shp = arr_pf.shape
+    # Create a new array to accommodate the 10 pf and 1 cf members
+    arr_comb = np.empty([arr_shp[0], arr_shp[1], arr_shp[2]+1, arr_shp[3], arr_shp[4]])
+    # Populate arr_comb with pf and cf data
+    arr_comb[:,:,0:10,:,:] = arr_pf
+
+    # Read in the cf's total precip ('tp') variable
+    arr_cf = prec_cf
+    arr_comb[:,:,10,:,:] = arr_cf
+
+    # Average over the ensemble axis/dimension
+    arr_ens_avg = np.mean(arr_comb, axis=2)
+
+    # Calculate weekly totals
+    arr_wkly_ttl = np.empty([arr_shp[0], arr_shp[1]-1, arr_shp[3], arr_shp[4]])
+    for i in range(arr_ens_avg.shape[1]-1):
+        arr_wkly_ttl[:,i,:,:] = np.subtract(arr_ens_avg[:,i+1,:,:], arr_ens_avg[:,i,:,:]) # Substract to get weekly totals
+        arr_wkly_ttl[:,i,:,:] = arr_wkly_ttl[:,i,:,:]/7.0 # Divide by 7 for daily average
+    #-------------------------------------
+    # Output variable to netCDF
+    #-------------------------------------
+    # Define output file destination
+    ds_out = netCDF4.Dataset(dest_dir + "ECMWF_prec_2016-" + init_date[i_date] + '_weekly.nc', 'w',format='NETCDF4_CLASSIC')
+
+    # Create all the required dimensions of the variable 
+    latitude = ds_out.createDimension('latitude', len(prec_lat))
+    longitude = ds_out.createDimension('longitude', len(prec_lon))
+    step = ds_out.createDimension('step', len(prec_st)-1) 
+    time = ds_out.createDimension('time', len(prec_hd))    
+
+    # Create the variables to store the data
+    times = ds_out.createVariable('time', 'f8', ('time',)) 
+    steps = ds_out.createVariable('step', 'f4', ('step',)) 
+    latitudes = ds_out.createVariable('latitude', 'f4', ('latitude',))
+    longitudes = ds_out.createVariable('longitude', 'f4', ('longitude',)) 
+    # Create the 4-d precipitation variable
+    precip = ds_out.createVariable('precip', 'f4', ('time','step','latitude','longitude',),) 
+
+    # Define the properties of the variables
+    times.units = 'hours since 2016-' + init_date[i_date] + ' 00:00:00.0';
+    times.calendar = 'standard';
+    latitudes.units= 'degrees_north'  
+    longitudes.units= 'degrees_east'  
+    precip.units = 'kg m**-2'
+    precip.missing_value= -32767
+    precip.long_name = 'Total Precipitation'
+
+    # Format the time variable
+    prec_time = [];
+    for i in range(0,len(prec_hd)):
+        prec_time.append(datetime.datetime(year=int(str(prec_hd[i])[0:4]), month=int(str(prec_hd[i])[4:6]), day=int(str(prec_hd[i])[6:8])))
+    
+    # Populate the variables
+    latitudes[:] = prec_lat   
+    longitudes[:] = prec_lon
+    times[:] = netCDF4.date2num(prec_time, units=times.units, calendar=times.calendar);
+    steps[:] = prec_st[1:5]  
+    precip[:,:,:,:] = arr_wkly_ttl[:,:,:,:]
+
+    # File is saved to .nc once closed
+    ds_out.close()  
+
+    ds_pf.close()
+    ds_cf.close()

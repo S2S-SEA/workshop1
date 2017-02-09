c75f3ddefddc4ade29e6be84202b352c4262aa39
jzanetti0 <nuistliujia@gmail.com>
2017-02-09 17:58:26 +0800

    add all functions

--- code/obs/prec/s2s_utility.py
+++ code/obs/prec/s2s_utility.py
@@ -31,7 +31,7 @@ def read_trmm(cur_trmm_path):
     data_time = netCDF4.num2date(time_var[:],time_var.units)
     trmm_lat = nc.variables['Y'][:]
     trmm_lon = nc.variables['X'][:]
-    trmm_data = prcpvar[:,:,:]
+    trmm_data = prcpvar[:,:,:]    #time,lat,lon
 
     #Store TRMM time
     trmm_time = []
@@ -56,6 +56,23 @@ def read_ec(cur_ec_path):
     ec_data = pdata[::-1,:,::-1,:]    #reserve time/lat
 
     return ec_time,ec_step,ec_data
+    nc.close()
+
+def read_anomaly(cur_data_path,index):
+
+    #Open data path to retreive data
+    nc = netCDF4.Dataset(cur_data_path)
+    prcpvar = nc.variables['rainfall']
+
+    #Read latitude and longitude and data
+    data_lat = nc.variables['latitude'][:]
+    data_lon = nc.variables['longitude'][:]
+    if index == 'TRMM':
+       pdata = prcpvar[:,:,:,:]    #week,year,lat,lon
+    if index == 'ECMWF':
+       pdata = prcpvar[:,:,:,:,:]    #step,week,year,lat,lon
+
+    return data_lat,data_lon,pdata
     nc.close()
 
 def write_trmm(trmm_output,trmm_filename,trmm_data,trmm_week,trmm_year,trmm_lat,trmm_lon,index):
@@ -143,7 +160,7 @@ def write_ec(ec_output,ec_filename,ec_data,ec_step,ec_week,ec_year,ec_lat,ec_lon
        rainfall[:,:,:,:] = ec_data
     nc.close()
 
-def plot_fig(data_0,lat_0,lon_0,lat_down,lat_up,lon_left,lon_right,grid_lat,grid_lon,data_range,title_str,name_str,index):
+def plot_processing(data_0,lat_0,lon_0,lat_down,lat_up,lon_left,lon_right,grid_lat,grid_lon,data_range,title_str,name_str,index):
 
     #Find borders of the domain
     L1,R1,L2,R2 = find_point(lat_0,lon_0,lat_down,lat_up,lon_left,lon_right)
@@ -186,6 +203,61 @@ def plot_fig(data_0,lat_0,lon_0,lat_down,lat_up,lon_left,lon_right,grid_lat,grid
     #Add colorbar
     cbar = m.colorbar(cs,location='bottom',pad="5%")
     cbar.set_label('mm/day',fontsize=13)
+
+    #Add title and save figures
+    plt.title(title_str,fontsize=13)
+    plt.savefig(name_str,dpi=200,bbox_inches='tight')
+    plt.close()
+
+def plot_verification(data_0,lat_0,lon_0,lat_down,lat_up,lon_left,lon_right,grid_lat,grid_lon,title_str,name_str,index):
+
+    #Find borders of the domain
+    L1,R1,L2,R2 = find_point(lat_0,lon_0,lat_down,lat_up,lon_left,lon_right)
+    lat = lat_0[L1:R1]
+    lon = lon_0[L2:R2]
+    latcorners = [lat.min(),lat.max()]
+    loncorners = [lon.min(),lon.max()]
+
+    #Add basemap
+    #For map resolution, there are five options: c(crude),l(low),i(intermediate),h(high),f(full)
+    #Higher resolution requires more time, defalut using low resolution
+    m = Basemap(projection='merc',llcrnrlon=loncorners[0],llcrnrlat=latcorners[0],urcrnrlon=loncorners[1],urcrnrlat=latcorners[1],resolution='l')
+    m.drawmapboundary()
+    m.drawcoastlines()
+    m.drawcountries()
+
+    #Draw parallels and meridians
+    parallels = numpy.arange(lat_down,lat_up,grid_lat)
+    m.drawparallels(parallels,labels=[1,0,0,0],fontsize=12)
+    meridians = numpy.arange(lon_left,lon_right,grid_lon)
+    m.drawmeridians(meridians,labels=[0,0,0,1],fontsize=12)
+
+    #Compute map proj coordinates
+    data = data_0[L1:R1,L2:R2]
+    ny = data.shape[0]
+    nx = data.shape[1]
+    lons,lats = m.makegrid(nx,ny)
+    x,y = m(lons,lats)
+
+    #Create discrete colormap
+    if index == 'CORA':
+       cmap = mpl.colors.ListedColormap([(0.7451,0.8627,1),(0.9216,0.9216,0.9216),(1,0.9804,0.4902),(1,0.8235,0.8235),(1,0.6863,0.6863),(1,0.4902,0.4902),(1,0.1529,0.1529),(0.8235,0,0),(0.5961,0,0),(0.4510,0,0),(0.2941,0,0)])
+       bounds = [-1,0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]
+    if index == 'MSSS':
+       cmap = mpl.colors.ListedColormap([(0.6863,0.6863,1),(0.7451,0.8627,1),(0.8235,0.8235,0.8235),(1,0.9294,0.9294),(1,0.8235,0.8235),(1,0.6863,0.6863),(1,0.4902,0.4902),(1,0.1569,0.1569),(0.8235,0,0),(0.5961,0,0)])
+       bounds = [data.min(),-1,0,0.05,0.1,0.2,0.3,0.4,0.5,0.6,1]
+    norm = mpl.colors.BoundaryNorm(bounds,cmap.N)
+
+    #Plotting
+    cs = m.pcolormesh(x,y,data,cmap=cmap,norm=norm)
+    #Add colorbar
+    cbar = m.colorbar(cs,location='bottom',pad="5%")
+    if index == 'CORA':
+       cbar.set_ticks([0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1])
+       cbar.set_ticklabels(['0','0.1','0.2','0.3','0.4','0.5','0.6','0.7','0.8','0.9','1'])
+    if index == 'MSSS':
+       cbar.set_ticks([-1,0,0.05,0.1,0.2,0.3,0.4,0.5,0.6,1])
+       cbar.set_ticklabels(['-1','0','0.05','0.1','0.2','0.3','0.4','0.5','0.6','1'])
 
     #Add title and save figures
     plt.title(title_str,fontsize=13)

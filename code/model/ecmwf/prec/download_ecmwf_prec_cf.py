ba067fb3e23966dc9cd95ea228aad2acc99d37e0
Raizan <raizan.rahmat@gmail.com>
2017-02-23 22:57:49 +0800

    Removed line to del files.

--- code/model/ecmwf/prec/download_ecmwf_prec_cf.py
+++ code/model/ecmwf/prec/download_ecmwf_prec_cf.py
@@ -15,7 +15,7 @@ dest_dir = '../../../../data/model/ecmwf/prec/'
 call("mkdir -p " + dest_dir, shell=True)
 
 # Remove all *cf.nc files, else grib_to_netcdf will convert with "protocol error"
-call("rm -rf " + dest_dir + "*_cf.nc", shell=True)
+#call("rm -rf " + dest_dir + "*_cf.nc", shell=True)
  
 # All the initial dates that have full weeks in Nov
 init_date = ['10-13','10-17','10-20','10-24','10-27','10-31','11-03','11-07','11-10','11-14','11-17','11-21','11-24']

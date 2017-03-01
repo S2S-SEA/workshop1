c4f5ecd7f6eb04994183af96759bd33455bd18cf
Turkington <thea_turkington@nea.gov.sg>
2017-03-01 04:14:45 +0100

    Add files via upload

---
+++ code/model/ecmwf/temp/plot_timeseries.py
@@ -0,0 +1,61 @@
+import os
+
+import numpy
+
+import netCDF4
+
+import matplotlib.pyplot as plt
+
+
+
+
+
+def plot_fig(trmm_anomaly,ec_anomaly,target_week,start_year,end_year,lat,lon,lat_down,lat_up,lon_left,lon_right,title_str,name_str):
+
+    
+
+
+#Put ECMWF and ECMWF for domain and week selected into list
+    
+    trmm_anomaly_list = []
+    
+    for i_year in range(0,trmm_anomaly.shape[1]):
+        
+	      trmm_anomaly_list.append(numpy.mean(trmm_anomaly[target_week,i_year,:,:]))
+    
+    
+    ec_anomaly_list = []
+    
+    for i_step in range(0,ec_anomaly.shape[0]):
+        
+	      for i_year in range(0,ec_anomaly.shape[2]):
+            
+
+	         ec_anomaly_list.append(numpy.mean(ec_anomaly[i_step,target_week,i_year,:,:]))
+    
+    
+
+
+	#Plot time series
+    
+    fig,ax = plt.subplots(figsize=(10,5))
+    
+    xx = range(start_year,end_year+1)
+    
+    ax.plot([start_year, end_year], [ 0,0], 'k' )
+    ax.plot(xx,trmm_anomaly_list,'--bo',linewidth=2,label='ERA-I')
+    
+    ax.plot(xx,ec_anomaly_list[0:end_year-start_year+1],linewidth=2,label='EC_LT1')
+    
+    ax.plot(xx,ec_anomaly_list[end_year-start_year+1:(end_year-start_year+1)*2],linewidth=2,label='EC_LT2')
+    ax.plot(xx,ec_anomaly_list[(end_year-start_year+1)*2:(end_year-start_year+1)*3],linewidth=2,label='EC_LT3')
+    ax.plot(xx,ec_anomaly_list[(end_year-start_year+1)*3:(end_year-start_year+1)*4],linewidth=2,label='EC_LT4')
+    #Add title and save figures
+    
+    plt.legend(loc='best')
+    
+    plt.title(title_str,fontsize=13)
+    
+    plt.savefig(name_str,dpi=200,bbox_inches='tight')
+    
+    plt.close()
---
+++ code/model/ecmwf/temp/process_timeseries.py
@@ -0,0 +1,58 @@
+
+import plot_timeseries
+
+import processFunctions as pf
+import s2s_utilities_temp as s2s
+
+##-----------------------------------------------------
+#		SPECIFY
+#------------------------------------------------------
+model_initial_date = ['1013','1017','1020','1024','1027','1031','1103','1107','1110','1114','1117','1121','1124'] # this is the name of all the ECMWF files for the start dates
+# make sure this list matches with the files you downloaded
+weeks = [3,7,10,14,17,21,24] # these are the start dates for the weeks you are looking for. Make sure they match with the model data!
+month = 11 # this is the month you want to be looking at 
+start_year = 1998 
+end_year = 2014 
+lead_times = 4 # this is the number of lead time weeks 
+ec_input = '../../../../data/model/ecmwf/temp' # will need to change this path 
+
+#the domain
+lat_up = 10
+lat_down = -10
+lon_left = 90
+lon_right = 110
+
+target_week  = 0
+
+
+#--------------------#This part is to prepare ERA-I and ECMWF anomaly data
+#---------------------------------------
+
+
+#get the Interim data (similar to process script)
+erai_lon, erai_lat, erai_dates, erai_anom= pf.processInterim(month, weeks, start_year, end_year, 3)
+LD, LU, LL, LR, erai_lat, erai_lon = s2s.makeSmaller(erai_lat, erai_lon, lat_up, lat_down, lon_left, lon_right)
+
+#Get the EC and EI data
+erai_anom = erai_anom[:,:,LD:LU+1,LL:LR+1]
+
+
+# get the Hindcast data Anomalies
+ec_lon, ec_lat, ec_anom = pf.processEcmwfTemp(ec_input, model_initial_date, lead_times, month, weeks, start_year, end_year, 3)
+LD, LU, LL, LR, ec_lat, ec_lon = s2s.makeSmaller(ec_lat, ec_lon, lat_up, lat_down, lon_left, lon_right)
+
+ec_anom = ec_anom[:,:,:,LD:LU+1,LL:LR+1]
+
+
+
+
+#------------------------
+#This part is to display time series of anomalies
+#---------------
+
+
+#
+title_str = 'Temperature Anomaly Time Series' + '\n' + str(weeks[target_week])+'/11'
+name_str = 'ECMWF_Temperature_Timeseries_' + str(weeks[target_week]) + '-' + str(month) + '.png'
+
+plot_timeseries.plot_fig(erai_anom,ec_anom,target_week,start_year,end_year,ec_lat,ec_lon,lat_down,lat_up,lon_left,lon_right,title_str,name_str)
